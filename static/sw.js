ba6df20ff38592de9db8e959a7a0bf5945334534
Arturo Guerra <ar2roguerra@gmail.com>
2018-04-08 18:53:40 +0000

    Clean up

--- static/sw.js
+++ static/sw.js
@@ -8,32 +8,32 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.19fa5e4ea57142c99fa2.js",
-    "revision": "07dc4bb46974cb1b372ae37446b00940"
+    "url": "/_nuxt/app.685221de7dfe01c733b2.js",
+    "revision": "a14894453fcb41af272ed13e5869797f"
   },
   {
-    "url": "/_nuxt/layouts/default.9977540d3541520c00d8.js",
-    "revision": "d6fc5fc8f58b8ef6a1fad1cac056a8a1"
+    "url": "/_nuxt/layouts/default.3c753c01d196e359f4c1.js",
+    "revision": "20b699f7f3ecf292fab6e32bef2503d9"
   },
   {
-    "url": "/_nuxt/manifest.263185a64bf42335217c.js",
-    "revision": "3fed234e44a8425effe14fa7117fa390"
+    "url": "/_nuxt/manifest.7d756eae8ede405d5fda.js",
+    "revision": "7bb8958ef86d84c4a47792937c078354"
   },
   {
-    "url": "/_nuxt/pages/apinfo.863985e809612cb8b2b8.js",
-    "revision": "9de4707b8594e032e75d8bbdf82a5d2e"
+    "url": "/_nuxt/pages/apinfo.2fcc8ad88bd0c9d8ee12.js",
+    "revision": "4e425eb49fba56f6fe06e9cc87fdf95e"
   },
   {
-    "url": "/_nuxt/pages/dixionary.99cc692d72d5fefd27c1.js",
-    "revision": "999692191aa3d6b9a43daa6f0dbbd7f2"
+    "url": "/_nuxt/pages/dixionary.cb30c9a46178c6610eb1.js",
+    "revision": "730ea1432bb8efc8e5535c24d63bb9f6"
   },
   {
-    "url": "/_nuxt/pages/index.f431eaceba597623d67a.js",
-    "revision": "faa4c820f96a3de4f33155cbb703c1e7"
+    "url": "/_nuxt/pages/index.c45d2332aacda9d8451b.js",
+    "revision": "b11395bbf3f39c9b2faccfe684e0cdc5"
   },
   {
-    "url": "/_nuxt/pages/translate.7e521edfccfb98afea51.js",
-    "revision": "4f3f1921def1f194af52d936c5417630"
+    "url": "/_nuxt/pages/translate.3bf0bb0f86230ecd221a.js",
+    "revision": "7c47633f58e26b284f9848d44943b684"
   },
   {
     "url": "/_nuxt/vendor.8e3649c62c9f89005f61.js",
