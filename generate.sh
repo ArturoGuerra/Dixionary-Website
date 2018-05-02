120cdd3057face43fc24abab5562e978203949b7
Arturo Guerra <ar2roguerra@gmail.com>
2018-05-02 20:56:07 +0000

    aws s3 acl

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd dist
 aws s3 rm --recursive s3://vvv.dixionary.com/
-aws s3 cp . s3://vvv.dixionary.com/ --recursive
+aws s3 cp . s3://vvv.dixionary.com/ --recursive --acl public-read
